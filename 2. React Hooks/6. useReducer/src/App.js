d24b38059147cff10416b6fa89abe3576bdf2ecd
newsarawut <sarawutch572@gmail.com>
2024-05-23 10:14:50 +0700

    useReducer

---
+++ 2. React Hooks/6. useReducer/src/App.js
@@ -0,0 +1,41 @@
+import { useReducer } from "react";
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <>
+    <h1>{state.count}</h1>
+    <button onClick={() => dispatch({type: "increment"})}>+</button>
+    <button onClick={() => dispatch({type: "decrement"})}>-</button>
+    <button onClick={() => dispatch({type: "reset"})}>Reset</button>
+    </>
+  );
+}
+
+const initialState = { count:0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "reset":
+      return {
+        ...state,
+        count: 0,
+      };
+
+      default:
+        return state;
+  }
+}
+
+export default App;
